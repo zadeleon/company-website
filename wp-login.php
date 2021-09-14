c43b4cece1683eec46f991a886125c85cd4ad6c6
MSI\Aleo <aleodeleon5@gmail.com>
2021-09-14 18:04:55 +0800

    no message

--- wp-login.php
+++ wp-login.php
@@ -5,11 +5,11 @@
 	<title>Log In &lsaquo; Whitelight &#8212; WordPress</title>
 	<meta name="robots" content="max-image-preview:large, noindex, noarchive">
 <link rel="dns-prefetch" href="//s.w.org">
-<link rel="stylesheet" id="dashicons-css" href="https://zadeleon.github.io/company-website.git/wp-includes/css/dashicons.min.css?ver=5.8.1" type="text/css" media="all">
-<link rel="stylesheet" id="buttons-css" href="https://zadeleon.github.io/company-website.git/wp-includes/css/buttons.min.css?ver=5.8.1" type="text/css" media="all">
-<link rel="stylesheet" id="forms-css" href="https://zadeleon.github.io/company-website.git/wp-admin/css/forms.min.css?ver=5.8.1" type="text/css" media="all">
-<link rel="stylesheet" id="l10n-css" href="https://zadeleon.github.io/company-website.git/wp-admin/css/l10n.min.css?ver=5.8.1" type="text/css" media="all">
-<link rel="stylesheet" id="login-css" href="https://zadeleon.github.io/company-website.git/wp-admin/css/login.min.css?ver=5.8.1" type="text/css" media="all">
+<link rel="stylesheet" id="dashicons-css" href="https://zadeleon.github.io/company-website/wp-includes/css/dashicons.min.css?ver=5.8.1" type="text/css" media="all">
+<link rel="stylesheet" id="buttons-css" href="https://zadeleon.github.io/company-website/wp-includes/css/buttons.min.css?ver=5.8.1" type="text/css" media="all">
+<link rel="stylesheet" id="forms-css" href="https://zadeleon.github.io/company-website/wp-admin/css/forms.min.css?ver=5.8.1" type="text/css" media="all">
+<link rel="stylesheet" id="l10n-css" href="https://zadeleon.github.io/company-website/wp-admin/css/l10n.min.css?ver=5.8.1" type="text/css" media="all">
+<link rel="stylesheet" id="login-css" href="https://zadeleon.github.io/company-website/wp-admin/css/login.min.css?ver=5.8.1" type="text/css" media="all">
 	<meta name="referrer" content="strict-origin-when-cross-origin">
 		<meta name="viewport" content="width=device-width">
 		</head>
@@ -20,7 +20,7 @@
 		<div id="login">
 		<h1><a href="https://wordpress.org/">Powered by WordPress</a></h1>
 	
-		<form name="loginform" id="loginform" action="https://zadeleon.github.io/company-website.git/wp-login.php" method="post">
+		<form name="loginform" id="loginform" action="https://zadeleon.github.io/company-website/wp-login.php" method="post">
 			<p>
 				<label for="user_login">Username or Email Address</label>
 				<input type="text" name="log" id="user_login" class="input" value="" size="20" autocapitalize="off">
@@ -38,33 +38,33 @@
 						<p class="forgetmenot"><input name="rememberme" type="checkbox" id="rememberme" value="forever"> <label for="rememberme">Remember Me</label></p>
 			<p class="submit">
 				<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Log In">
-									<input type="hidden" name="redirect_to" value="https://zadeleon.github.io/company-website.git/wp-admin/">
+									<input type="hidden" name="redirect_to" value="https://zadeleon.github.io/company-website/wp-admin/">
 									<input type="hidden" name="testcookie" value="1">
 			</p>
 		</form>
 
 					<p id="nav">
-								<a href="https://zadeleon.github.io/company-website.git/wp-login.php?action=lostpassword">Lost your password?</a>
+								<a href="https://zadeleon.github.io/company-website/wp-login.php?action=lostpassword">Lost your password?</a>
 			</p>
 					<script type="text/javascript">
 			function wp_attempt_focus() {setTimeout( function() {try {d = document.getElementById( "user_login" );d.focus(); d.select();} catch( er ) {}}, 200);}
 wp_attempt_focus();
 if ( typeof wpOnload === 'function' ) { wpOnload() }		</script>
 				<p id="backtoblog">
-			<a href="https://zadeleon.github.io/company-website.git/">&larr; Go to Whitelight</a>		</p>
+			<a href="https://zadeleon.github.io/company-website/">&larr; Go to Whitelight</a>		</p>
 			</div>
-	<script type="text/javascript" src="https://zadeleon.github.io/company-website.git/wp-includes/js/jquery/jquery.min.js?ver=3.6.0" id="jquery-core-js"></script>
-<script type="text/javascript" src="https://zadeleon.github.io/company-website.git/wp-includes/js/jquery/jquery-migrate.min.js?ver=3.3.2" id="jquery-migrate-js"></script>
+	<script type="text/javascript" src="https://zadeleon.github.io/company-website/wp-includes/js/jquery/jquery.min.js?ver=3.6.0" id="jquery-core-js"></script>
+<script type="text/javascript" src="https://zadeleon.github.io/company-website/wp-includes/js/jquery/jquery-migrate.min.js?ver=3.3.2" id="jquery-migrate-js"></script>
 <script type="text/javascript" id="zxcvbn-async-js-extra">
 /*  */
-var _zxcvbnSettings = {"src":"https:\/\/zadeleon.github.io\/company-website.git\/wp-includes\/js\/zxcvbn.min.js"};
+var _zxcvbnSettings = {"src":"https:\/\/zadeleon.github.io\/company-website\/wp-includes\/js\/zxcvbn.min.js"};
 /*  */
 </script>
-<script type="text/javascript" src="https://zadeleon.github.io/company-website.git/wp-includes/js/zxcvbn-async.min.js?ver=1.0" id="zxcvbn-async-js"></script>
-<script type="text/javascript" src="https://zadeleon.github.io/company-website.git/wp-includes/js/dist/vendor/regenerator-runtime.min.js?ver=0.13.7" id="regenerator-runtime-js"></script>
-<script type="text/javascript" src="https://zadeleon.github.io/company-website.git/wp-includes/js/dist/vendor/wp-polyfill.min.js?ver=3.15.0" id="wp-polyfill-js"></script>
-<script type="text/javascript" src="https://zadeleon.github.io/company-website.git/wp-includes/js/dist/hooks.min.js?ver=a7edae857aab69d69fa10d5aef23a5de" id="wp-hooks-js"></script>
-<script type="text/javascript" src="https://zadeleon.github.io/company-website.git/wp-includes/js/dist/i18n.min.js?ver=5f1269854226b4dd90450db411a12b79" id="wp-i18n-js"></script>
+<script type="text/javascript" src="https://zadeleon.github.io/company-website/wp-includes/js/zxcvbn-async.min.js?ver=1.0" id="zxcvbn-async-js"></script>
+<script type="text/javascript" src="https://zadeleon.github.io/company-website/wp-includes/js/dist/vendor/regenerator-runtime.min.js?ver=0.13.7" id="regenerator-runtime-js"></script>
+<script type="text/javascript" src="https://zadeleon.github.io/company-website/wp-includes/js/dist/vendor/wp-polyfill.min.js?ver=3.15.0" id="wp-polyfill-js"></script>
+<script type="text/javascript" src="https://zadeleon.github.io/company-website/wp-includes/js/dist/hooks.min.js?ver=a7edae857aab69d69fa10d5aef23a5de" id="wp-hooks-js"></script>
+<script type="text/javascript" src="https://zadeleon.github.io/company-website/wp-includes/js/dist/i18n.min.js?ver=5f1269854226b4dd90450db411a12b79" id="wp-i18n-js"></script>
 <script type="text/javascript" id="wp-i18n-js-after">
 wp.i18n.setLocaleData( { 'text directionltr': [ 'ltr' ] } );
 </script>
@@ -80,14 +80,14 @@ var pwsL10n = {"unknown":"Password strength unknown","short":"Very weak","bad":"
 	wp.i18n.setLocaleData( localeData, domain );
 } )( "default", { "locale_data": { "messages": { "": {} } } } );
 </script>
-<script type="text/javascript" src="https://zadeleon.github.io/company-website.git/wp-admin/js/password-strength-meter.min.js?ver=5.8.1" id="password-strength-meter-js"></script>
-<script type="text/javascript" src="https://zadeleon.github.io/company-website.git/wp-includes/js/underscore.min.js?ver=1.13.1" id="underscore-js"></script>
+<script type="text/javascript" src="https://zadeleon.github.io/company-website/wp-admin/js/password-strength-meter.min.js?ver=5.8.1" id="password-strength-meter-js"></script>
+<script type="text/javascript" src="https://zadeleon.github.io/company-website/wp-includes/js/underscore.min.js?ver=1.13.1" id="underscore-js"></script>
 <script type="text/javascript" id="wp-util-js-extra">
 /*  */
 var _wpUtilSettings = {"ajax":{"url":"\/wordpress\/wp-admin\/admin-ajax.php"}};
 /*  */
 </script>
-<script type="text/javascript" src="https://zadeleon.github.io/company-website.git/wp-includes/js/wp-util.min.js?ver=5.8.1" id="wp-util-js"></script>
+<script type="text/javascript" src="https://zadeleon.github.io/company-website/wp-includes/js/wp-util.min.js?ver=5.8.1" id="wp-util-js"></script>
 <script type="text/javascript" id="user-profile-js-extra">
 /*  */
 var userProfileL10n = {"user_id":"0","nonce":"125649860c"};
@@ -100,7 +100,7 @@ var userProfileL10n = {"user_id":"0","nonce":"125649860c"};
 	wp.i18n.setLocaleData( localeData, domain );
 } )( "default", { "locale_data": { "messages": { "": {} } } } );
 </script>
-<script type="text/javascript" src="https://zadeleon.github.io/company-website.git/wp-admin/js/user-profile.min.js?ver=5.8.1" id="user-profile-js"></script>
+<script type="text/javascript" src="https://zadeleon.github.io/company-website/wp-admin/js/user-profile.min.js?ver=5.8.1" id="user-profile-js"></script>
 	<div class="clear"></div>
 	</body>
 	</html>
